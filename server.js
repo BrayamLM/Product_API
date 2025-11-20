102006eeb91a88e13c24eeadcb453200b7c5a0a6
Brayam Lopez <brayam0055@gmail.com>
2025-11-20 16:10:49 -0600

    Fix load size

--- server.js
+++ server.js
@@ -43,7 +43,8 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Middleware
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // MongoDB Connection
 const MONGODB_URI = process.env.MONGODB_URI;
